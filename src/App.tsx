191d224e1ca590bde378d63af26d67f5e7279a62
mazurenka <an.drej@mail.ru>
2022-08-19 17:52:12 +0200

    01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            This is App component
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </div>
+    )
 }
 
 export default App;
