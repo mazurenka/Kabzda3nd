edc9d098bbd716236a27e2175374032de9da1b4d
mazurenka <an.drej@mail.ru>
2022-08-26 19:18:02 +0200

    03

---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function Rating(props: any) {
+    console.log('Rating rendering')
+    return (
+        <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+        </div>
+    )
+}
+
+function Star(props: any) {
+    console.log('Star rendering')
+
+    if (props.selected === true) {
+        return <span><b>star</b> </span>
+    } else {
+        return <span>star </span>
+    }
+}
